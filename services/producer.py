9b2841b4a302dae49fcca1cf7c2c191be4e0dc8e
funnymilk <andre.tum91@gmail.com>
2025-11-04 12:28:02 +0300

    kafka+yasender

---
+++ services/producer.py
@@ -0,0 +1,37 @@
+from confluent_kafka import Producer
+import json
+
+# Конфигурация Kafka Producer
+# пароль от Яндекс почты wpcuomeiqpbkewcw
+
+conf = {
+    'bootstrap.servers': 'kafka:9092',  # адрес Kafka broker
+    'client.id': 'todo-producer'
+}
+
+producer = Producer(conf)
+
+def delivery_report(err, msg):
+    """Callback для подтверждения доставки сообщения"""
+    if err is not None:
+        print(f'Ошибка доставки сообщения: {err}')
+    else:
+        print(f'Сообщение доставлено в {msg.topic()} [{msg.partition()}] по offset {msg.offset()}')
+
+def send_task_email(topic, name: str, user_email: str):
+    """Функция отправки события с задачей для email в Kafka"""
+    #topic = 'todo-email-kafka'
+    message = {
+        'task_name': name,
+        'email': user_email,
+        'subject': 'Уведомление о регистрации',
+        'body': f'Пользователь с почтовым адресом {user_email} успешно создан!'
+    }
+    for i in range(1):
+        producer.produce(
+        topic=topic,
+        value=json.dumps(message),
+        on_delivery=delivery_report
+    )
+        producer.flush()  # Гарантируем отправку перед завершением функции
+
